07f14a0508a66eb9d5ed516af9fca7651dc0b53e
Charissa Beaty <char.beaty11@gmail.com>
2019-12-30 17:35:16 -0700

    js added to server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("div h3").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
